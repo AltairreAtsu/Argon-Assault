728fc2dff7a7cd5ea84f4f1a6c5d98b6ea7c8395
Marc Taurasi <altairre.atsu@gmail.com>
2018-04-07 11:18:18 -0600

    Imported TextMeshPro and Music from Unity Developer Course. Created Splashscreen scene.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	[SerializeField] [Tooltip ("Delay in seconds the game waits on the Splash Screen.")]
+	private float splashScreenDelay = 3f;
+
+	private void Start () {
+		int buildIndex = SceneManager.GetActiveScene().buildIndex;
+
+		byte splashScreenIndex = 0;
+		if(buildIndex == splashScreenIndex)
+		{
+			Invoke("LoadNextLevel", splashScreenDelay);
+		}
+	}
+	
+	public void LoadNextLevel () {
+		int buildIndex = SceneManager.GetActiveScene().buildIndex;
+
+		SceneManager.LoadScene(buildIndex + 1);
+	}
+
+	public void LoadScene(int sceneToLoad)
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
